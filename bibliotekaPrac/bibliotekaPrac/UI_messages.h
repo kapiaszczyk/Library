d6c34e99de0757c55f6e59b8b915f1129c28934e
kapiaszczyk <ka.piaszczyk@gmail.com>
2022-05-26 14:48:04 +0200

    Split UI_messages into .hpp and .cpp

--- bibliotekaPrac/bibliotekaPrac/UI_messages.h
+++ bibliotekaPrac/bibliotekaPrac/UI_messages.h
@@ -2,76 +2,20 @@
 #ifndef UI_MESSAGESS_SEEN
 #define UI_MESSAGESS_SEEN
 
+class Praca;
+
 extern const std::vector<std::string> dozwoloneTypy;
 
-void menuMessage() {
+extern void printTypeList(const std::vector<std::string> dozwoloneTypy);
 
-    std::cout << "Wybierz funkcje programu. Aby wyjsc z programu, nacisnij X." << std::endl;
-    std::cout << "[1] Dodawanie pracy" << std::endl;
-    std::cout << "[2] Edycja pracy" << std::endl;
-    std::cout << "[3] Wyswietlanie listy prac w formacie skroconym" << std::endl;
-    std::cout << "[4] Wyswietlanie listy prac w formacie pelnym" << std::endl;
-    std::cout << "[5] Wyszukiwanie pracy" << std::endl;
-    std::cout << "[6] Dodatkowe funkcje" << std::endl;
-    std::cout << "[X] Wyjscie z programu" << std::endl;
+extern void menuMessage();
 
-}
+extern void subMenuMessage();
 
-void subMenuMessage() {
-    std::cout << "Wybierz dodatkowe funkcje programu. Aby wyjsc z submenu, nacisnij X" << std::endl;
-    std::cout << "[1] Wyswietlanie liczby wszystkich prac" << std::endl;
-    std::cout << "[2] Wyswietlanie liczby prac dla poszczegolnych promotorow" << std::endl;
-    std::cout << "[3] Wyswietlanie 10 slow kluczowych" << std::endl;
-    std::cout << "[4] Wyswietlanie 10 slow kluczowych" << std::endl;
-    std::cout << "[X] Wyjscie z dodatkowych funkcji" << std::endl;
-}
+extern void searchMenuMessage();
 
-void searchMenuMessage() {
+extern void editMessage();
 
-    std::cout << "Na podstawie jakiego kryterium chcesz wyszukac prace?" << std::endl;
-    std::cout << "[1] Nazwisko autora" << std::endl;
-    std::cout << "[2] Nazwisko promotora" << std::endl;
-    std::cout << "[3] Tytul" << std::endl;
-    std::cout << "[4] Slowo kluczowe" << std::endl;
-    std::cout << "[5] Streszczenie" << std::endl;
-    std::cout << "[6] Rok powstania" << std::endl;
-    std::cout << "[X] Wyjscie z wyszukiwania" << std::endl;
-
-}
-
-void editMessage() {
-    std::cout << "Ktora wartosc chcesz edytowac?" << std::endl;
-
-    std::cout << "[1] Nazwisko autora" << std::endl;
-    std::cout << "[2] Nazwisko promotora" << std::endl;
-    std::cout << "[3] Tytul" << std::endl;
-    std::cout << "[4] Slowo kluczowe" << std::endl;
-    std::cout << "[5] Streszczenie" << std::endl;
-    std::cout << "[3] Rok powstania" << std::endl;
-}
-
-
-void typPracyMessage(Praca::messageFlag flag) {
-
-    switch (flag) {
-    case Praca::initial:
-        std::cout << "Dodaj typ pracy." << std::endl;
-        break;
-    case Praca::error:
-        std::cout << "Niedozwolony typ pracy." << std::endl;
-        break;
-    case Praca::allowedValue:
-        std::cout << " Dozwolone typy pracy to: " << std::endl;
-        printTypeList(dozwoloneTypy);
-        break;
-    default:
-        break;
-    }
-}
-
-static void printTypeList(const std::vector<std::string> dozwoloneTypy) {
-    for (unsigned int i = 0; i < dozwoloneTypy.size(); i++) std::cout << dozwoloneTypy[i] << std::endl;
-}
-
+extern void typPracyMessage(Praca::messageFlag flag);
 
 #endif
